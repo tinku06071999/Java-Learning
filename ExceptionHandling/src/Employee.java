4129c0de618816458a65ebb72abb4ad66d6f4086
tinku06071999 <20bcs099@nith.com>
2026-01-30 17:28:06 +0530

    Adding Exception Handling Learning

---
+++ ExceptionHandling/src/Employee.java
@@ -0,0 +1,20 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Employee {
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
